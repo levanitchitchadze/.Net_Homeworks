7d99260c2e92c05c105aba1eb48b525dab5925ea
levanitchitchadze <61050876+levanitchitchadze@users.noreply.github.com>
2023-12-16 02:49:05 +0400

    add Homework7

---
+++ HomeWork7/HomeWork7/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace HomeWork7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroGrid = new MetroFramework.Controls.MetroGrid();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.InsertButton = new MetroFramework.Controls.MetroButton();
+            this.FirstNameL = new MetroFramework.Controls.MetroLabel();
+            this.FirstNameTB = new MetroFramework.Controls.MetroTextBox();
+            this.LastNameL = new MetroFramework.Controls.MetroLabel();
+            this.LastNameTB = new MetroFramework.Controls.MetroTextBox();
+            this.GenderL = new MetroFramework.Controls.MetroLabel();
+            this.SalaryL = new MetroFramework.Controls.MetroLabel();
+            this.SalaryTB = new MetroFramework.Controls.MetroTextBox();
+            this.DepartmentL = new MetroFramework.Controls.MetroLabel();
+            this.GenderCB = new MetroFramework.Controls.MetroComboBox();
+            this.DepartmentCB = new MetroFramework.Controls.MetroComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid)).BeginInit();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroGrid
+            // 
+            this.metroGrid.AllowUserToResizeRows = false;
+            this.metroGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.metroGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.metroGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.metroGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.metroGrid.DefaultCellStyle = dataGridViewCellStyle5;
+            this.metroGrid.EnableHeadersVisualStyles = false;
+            this.metroGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.metroGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid.Location = new System.Drawing.Point(3, 77);
+            this.metroGrid.Name = "metroGrid";
+            this.metroGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.metroGrid.RowHeadersWidth = 51;
+            this.metroGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.metroGrid.RowTemplate.Height = 24;
+            this.metroGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.metroGrid.ShowEditingIcon = false;
+            this.metroGrid.Size = new System.Drawing.Size(789, 594);
+            this.metroGrid.TabIndex = 0;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.DepartmentCB);
+            this.metroPanel1.Controls.Add(this.GenderCB);
+            this.metroPanel1.Controls.Add(this.SalaryTB);
+            this.metroPanel1.Controls.Add(this.LastNameTB);
+            this.metroPanel1.Controls.Add(this.FirstNameTB);
+            this.metroPanel1.Controls.Add(this.DepartmentL);
+            this.metroPanel1.Controls.Add(this.SalaryL);
+            this.metroPanel1.Controls.Add(this.GenderL);
+            this.metroPanel1.Controls.Add(this.LastNameL);
+            this.metroPanel1.Controls.Add(this.FirstNameL);
+            this.metroPanel1.Controls.Add(this.InsertButton);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(809, 77);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(296, 328);
+            this.metroPanel1.TabIndex = 1;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(47, 289);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(75, 23);
+            this.InsertButton.TabIndex = 2;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseSelectable = true;
+            this.InsertButton.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // FirstNameL
+            // 
+            this.FirstNameL.AutoSize = true;
+            this.FirstNameL.Location = new System.Drawing.Point(34, 36);
+            this.FirstNameL.Name = "FirstNameL";
+            this.FirstNameL.Size = new System.Drawing.Size(72, 20);
+            this.FirstNameL.TabIndex = 3;
+            this.FirstNameL.Text = "FirstName";
+            // 
+            // FirstNameTB
+            // 
+            // 
+            // 
+            // 
+            this.FirstNameTB.CustomButton.Image = null;
+            this.FirstNameTB.CustomButton.Location = new System.Drawing.Point(108, 1);
+            this.FirstNameTB.CustomButton.Name = "";
+            this.FirstNameTB.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.FirstNameTB.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.FirstNameTB.CustomButton.TabIndex = 1;
+            this.FirstNameTB.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.FirstNameTB.CustomButton.UseSelectable = true;
+            this.FirstNameTB.CustomButton.Visible = false;
+            this.FirstNameTB.Lines = new string[0];
+            this.FirstNameTB.Location = new System.Drawing.Point(140, 32);
+            this.FirstNameTB.MaxLength = 32767;
+            this.FirstNameTB.Name = "FirstNameTB";
+            this.FirstNameTB.PasswordChar = '\0';
+            this.FirstNameTB.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.FirstNameTB.SelectedText = "";
+            this.FirstNameTB.SelectionLength = 0;
+            this.FirstNameTB.SelectionStart = 0;
+            this.FirstNameTB.ShortcutsEnabled = true;
+            this.FirstNameTB.Size = new System.Drawing.Size(130, 23);
+            this.FirstNameTB.TabIndex = 4;
+            this.FirstNameTB.UseSelectable = true;
+            this.FirstNameTB.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.FirstNameTB.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // LastNameL
+            // 
+            this.LastNameL.AutoSize = true;
+            this.LastNameL.Location = new System.Drawing.Point(34, 83);
+            this.LastNameL.Name = "LastNameL";
+            this.LastNameL.Size = new System.Drawing.Size(71, 20);
+            this.LastNameL.TabIndex = 3;
+            this.LastNameL.Text = "LastName";
+            // 
+            // LastNameTB
+            // 
+            // 
+            // 
+            // 
+            this.LastNameTB.CustomButton.Image = null;
+            this.LastNameTB.CustomButton.Location = new System.Drawing.Point(108, 1);
+            this.LastNameTB.CustomButton.Name = "";
+            this.LastNameTB.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.LastNameTB.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.LastNameTB.CustomButton.TabIndex = 1;
+            this.LastNameTB.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.LastNameTB.CustomButton.UseSelectable = true;
+            this.LastNameTB.CustomButton.Visible = false;
+            this.LastNameTB.Lines = new string[0];
+            this.LastNameTB.Location = new System.Drawing.Point(140, 79);
+            this.LastNameTB.MaxLength = 32767;
+            this.LastNameTB.Name = "LastNameTB";
+            this.LastNameTB.PasswordChar = '\0';
+            this.LastNameTB.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.LastNameTB.SelectedText = "";
+            this.LastNameTB.SelectionLength = 0;
+            this.LastNameTB.SelectionStart = 0;
+            this.LastNameTB.ShortcutsEnabled = true;
+            this.LastNameTB.Size = new System.Drawing.Size(130, 23);
+            this.LastNameTB.TabIndex = 4;
+            this.LastNameTB.UseSelectable = true;
+            this.LastNameTB.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.LastNameTB.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // GenderL
+            // 
+            this.GenderL.AutoSize = true;
+            this.GenderL.Location = new System.Drawing.Point(34, 138);
+            this.GenderL.Name = "GenderL";
+            this.GenderL.Size = new System.Drawing.Size(56, 20);
+            this.GenderL.TabIndex = 3;
+            this.GenderL.Text = "Gender";
+            // 
+            // SalaryL
+            // 
+            this.SalaryL.AutoSize = true;
+            this.SalaryL.Location = new System.Drawing.Point(34, 187);
+            this.SalaryL.Name = "SalaryL";
+            this.SalaryL.Size = new System.Drawing.Size(46, 20);
+            this.SalaryL.TabIndex = 3;
+            this.SalaryL.Text = "Salary";
+            // 
+            // SalaryTB
+            // 
+            // 
+            // 
+            // 
+            this.SalaryTB.CustomButton.Image = null;
+            this.SalaryTB.CustomButton.Location = new System.Drawing.Point(108, 1);
+            this.SalaryTB.CustomButton.Name = "";
+            this.SalaryTB.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.SalaryTB.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.SalaryTB.CustomButton.TabIndex = 1;
+            this.SalaryTB.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.SalaryTB.CustomButton.UseSelectable = true;
+            this.SalaryTB.CustomButton.Visible = false;
+            this.SalaryTB.Lines = new string[0];
+            this.SalaryTB.Location = new System.Drawing.Point(140, 183);
+            this.SalaryTB.MaxLength = 32767;
+            this.SalaryTB.Name = "SalaryTB";
+            this.SalaryTB.PasswordChar = '\0';
+            this.SalaryTB.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SalaryTB.SelectedText = "";
+            this.SalaryTB.SelectionLength = 0;
+            this.SalaryTB.SelectionStart = 0;
+            this.SalaryTB.ShortcutsEnabled = true;
+            this.SalaryTB.Size = new System.Drawing.Size(130, 23);
+            this.SalaryTB.TabIndex = 4;
+            this.SalaryTB.UseSelectable = true;
+            this.SalaryTB.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.SalaryTB.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // DepartmentL
+            // 
+            this.DepartmentL.AutoSize = true;
+            this.DepartmentL.Location = new System.Drawing.Point(34, 232);
+            this.DepartmentL.Name = "DepartmentL";
+            this.DepartmentL.Size = new System.Drawing.Size(88, 20);
+            this.DepartmentL.TabIndex = 3;
+            this.DepartmentL.Text = "Department ";
+            // 
+            // GenderCB
+            // 
+            this.GenderCB.FormattingEnabled = true;
+            this.GenderCB.ItemHeight = 24;
+            this.GenderCB.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderCB.Location = new System.Drawing.Point(140, 128);
+            this.GenderCB.Name = "GenderCB";
+            this.GenderCB.Size = new System.Drawing.Size(130, 30);
+            this.GenderCB.TabIndex = 5;
+            this.GenderCB.UseSelectable = true;
+            // 
+            // DepartmentCB
+            // 
+            this.DepartmentCB.FormattingEnabled = true;
+            this.DepartmentCB.ItemHeight = 24;
+            this.DepartmentCB.Items.AddRange(new object[] {
+            "IT ",
+            "HR",
+            "Payroll"});
+            this.DepartmentCB.Location = new System.Drawing.Point(140, 222);
+            this.DepartmentCB.Name = "DepartmentCB";
+            this.DepartmentCB.Size = new System.Drawing.Size(130, 30);
+            this.DepartmentCB.TabIndex = 5;
+            this.DepartmentCB.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 733);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroGrid);
+            this.Name = "Form1";
+            this.Text = "Crud With Model First";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid)).EndInit();
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid metroGrid;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTextBox SalaryTB;
+        private MetroFramework.Controls.MetroTextBox LastNameTB;
+        private MetroFramework.Controls.MetroTextBox FirstNameTB;
+        private MetroFramework.Controls.MetroLabel DepartmentL;
+        private MetroFramework.Controls.MetroLabel SalaryL;
+        private MetroFramework.Controls.MetroLabel GenderL;
+        private MetroFramework.Controls.MetroLabel LastNameL;
+        private MetroFramework.Controls.MetroLabel FirstNameL;
+        private MetroFramework.Controls.MetroButton InsertButton;
+        private MetroFramework.Controls.MetroComboBox DepartmentCB;
+        private MetroFramework.Controls.MetroComboBox GenderCB;
+    }
+}
+
---
+++ HomeWork7/HomeWork7/Form1.cs
@@ -0,0 +1,72 @@
+﻿using MetroFramework.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        EmployeeDataContainer context = new EmployeeDataContainer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            var Employees=context.Employees.ToList();
+
+
+            
+
+            metroGrid.DataSource = Employees;
+            
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            int Department = 1;
+
+            switch (DepartmentCB.Text)
+            {
+                case "IT": Department = 1;
+                    break;
+                case "HR": Department = 2;
+                    break;
+                case "Payroll": Department = 3;
+                    break;
+            }
+
+
+
+
+            var employee = new Employee()
+            {
+
+                FirstName = FirstNameTB.Text,
+                LastName = LastNameTB.Text,
+                Gender = GenderCB.Text,
+                Salary = SalaryTB.Text,
+                DepartmentId = Department
+
+            };
+
+            context.Employees.Add(employee);
+            context.SaveChanges();
+            var Employees = context.Employees.ToList();
+            metroGrid.DataSource = Employees;
+
+        }
+    }
+}
